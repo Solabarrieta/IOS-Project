7cc9aa202230faf5499cf0e036a357c9cfbb5b45
illuminatus503 <davidcncmarcos@gmail.com>
2021-05-15 22:13:59 +0200

    Recognizer now recognizes words!

--- src/recognizer.c
+++ src/recognizer.c
@@ -6,7 +6,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <ctype.h>
 
+char *to_lowercase(char *word)
+{
+    char *res = (char *)malloc(strlen(word));
+
+    for (int i = 0; i < strlen(word); i++)
+    {
+        res[i] = (char)tolower(word[i]);
+    }
+
+    return res;
+}
+
+/**
+ * @brief Wait, util enter is pressed.
+ * 
+ */
 void wait_until_enter()
 {
     do
@@ -15,69 +32,293 @@ void wait_until_enter()
     } while (getchar() != 10);
 }
 
+int is_command(char *cmd)
+{
+    int n = 12;
+    char *path[12] = {"cat", "cp", "grep", "help", "ls", "man", "mv", "pwd", "touch", "stee", "exit", "cd"};
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!strcmp(cmd, path[i]))
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
 int read_doc(char *filename)
 {
-    int main_fd = open(filename, O_RDONLY);
-    int secd_fd = open(filename, O_RDONLY);
+    char character, *word, text[10000], *split[10000];
+    int i, j, main_fd = open(filename, O_RDONLY);
 
-    char character, word, *taboo[2] = {"Badwitch", "Goodwitch"};
-    int i, broken;
-
-    while (read(main_fd, &character, 1) == 1)
+    // Process the text and extract the speech.
+    for (i = 0; i < 9999; i++)
     {
-        i = 0;
-        broken = 0;
+        text[i] = ' ';
+    }
 
-        read(secd_fd, &word, 1);
+    text[9999] = '\0';
 
-        while (!((word == ',' || word == '.' || word == '?' || word == '!' || word == '\n' || word == ' ')))
-        {
-            if (word != taboo[0][i++])
-            {
-                broken = 1;
-                break;
-            }
+    i = 0;
+    while (read(main_fd, &character, 1) == 1 || character == '\n')
+    {
+        text[i++] = character;
+    }
 
-            read(secd_fd, &word, 1);
-        }
+    close(main_fd);
 
-        if (!broken && (i == strlen(taboo[0])))
+    // Split all the words by spaces.
+    for (i = 0; i < 10000; i++)
+    {
+        if (i == 0)
         {
-            print(OFELIA);
-            lseek(main_fd, i - 1, SEEK_CUR);
-            lseek(secd_fd, strlen(OFELIA), SEEK_CUR);
-        }
-
-        if (character == '_')
-        {
-            write(1, ADMIN, 7);
-        }
-        else if (character == '*')
-        {
-            write(1, OFELIA, strlen(OFELIA));
-        }
-        else if (character == '%')
-        {
-            write(1, GLINDA, strlen(GLINDA));
-        }
-        else if (character == '$')
-        {
-            write(1, SCARECROWN, strlen(SCARECROWN));
+            split[i] = strtok(text, " \n");
         }
         else
         {
-            write(1, &character, 1);
+            split[i] = strtok(NULL, " \n");
+        }
 
-            if (character == '.' || character == '!' || character == '?')
+        if (split[i] == NULL)
+        {
+            break;
+        }
+    }
+
+    char begin;
+
+    // Then, process the speech.
+    for (j = 0; j < i; j++)
+    {
+        word = split[j];
+        character = split[j][strlen(split[j]) - 1];
+        begin = split[j][0];
+
+        if (!strcmp(to_lowercase(word), "badwitch"))
+        {
+            print(OFELIA);
+        }
+        else if (!strcmp(to_lowercase(word), "goodwitch"))
+        {
+            print(GLINDA);
+        }
+        else if (!strcmp(to_lowercase(word), "admin"))
+        {
+            print(ADMIN);
+        }
+        else if (!strcmp(to_lowercase(word), "scarecrow"))
+        {
+            print(SCARECROWN);
+        }
+        else if (!strcmp(to_lowercase(word), "tinman"))
+        {
+            print(TINMAN);
+        }
+        else if (!strcmp(to_lowercase(word), "lion"))
+        {
+            print(LION);
+        }
+        else if (!strcmp(to_lowercase(word), "dog") || !strcmp(to_lowercase(word), "toto"))
+        {
+            print(DOG);
+        }
+        else if (!strcmp(to_lowercase(word), "trees"))
+        {
+            print(TREES);
+        }
+        else if (!strcmp(to_lowercase(word), "guardian"))
+        {
+            print(GUARDIAN);
+        }
+        else if (!strcmp(to_lowercase(word), "ghost") || !strcmp(to_lowercase(word), "casper"))
+        {
+            print(GHOST);
+        }
+        else if (is_command(to_lowercase(word)))
+        {
+            if (!strcmp(to_lowercase(word), "cat"))
             {
-                write(1, "\n", 1);
-                wait_until_enter();
+                print(concat(ANSI_COLOR_MAGENTA, "CAT"));
             }
+            else if (!strcmp(to_lowercase(word), "cp"))
+            {
+                print(concat(ANSI_COLOR_MAGENTA, "CP"));
+            }
+            else if (!strcmp(to_lowercase(word), "grep"))
+            {
+                print(concat(ANSI_COLOR_MAGENTA, "GREP"));
+            }
+            else if (!strcmp(to_lowercase(word), "help"))
+            {
+                print(concat(ANSI_COLOR_GREEN, "HELP"));
+            }
+            else if (!strcmp(to_lowercase(word), "ls"))
+            {
+                print(concat(ANSI_COLOR_YELLOW, "LS"));
+            }
+            else if (!strcmp(to_lowercase(word), "man"))
+            {
+                print(concat(ANSI_COLOR_GREEN, "MAN"));
+            }
+            else if (!strcmp(to_lowercase(word), "mv"))
+            {
+                print(concat(ANSI_COLOR_MAGENTA, "MV"));
+            }
+            else if (!strcmp(to_lowercase(word), "pwd"))
+            {
+                print(concat(ANSI_COLOR_YELLOW, "PWD"));
+            }
+            else if (!strcmp(to_lowercase(word), "touch"))
+            {
+                print(concat(ANSI_COLOR_BLUE, "TOUCH"));
+            }
+            else if (!strcmp(to_lowercase(word), "stee"))
+            {
+                print(concat(ANSI_COLOR_WHITE, "STEE"));
+            }
+            else if (!strcmp(to_lowercase(word), "exit"))
+            {
+                print(concat(ANSI_COLOR_RED, "EXIT"));
+            }
+            else if (!strcmp(to_lowercase(word), "cd"))
+            {
+                print(concat(ANSI_COLOR_CYAN, "CD"));
+            }
+        }
+        else
+        {
+            word = (char *)malloc(strlen(split[j]) - 1);
+
+            for (int k = 0; k < strlen(split[j]) - 1; k++)
+            {
+                word[k] = split[j][k];
+            }
+
+            if (!strcmp(to_lowercase(word), "badwitch"))
+            {
+                print(OFELIA);
+                write(1, &character, 1);
+            }
+            else if (!strcmp(to_lowercase(word), "goodwitch"))
+            {
+                print(GLINDA);
+                write(1, &character, 1);
+            }
+            else if (!strcmp(to_lowercase(word), "admin"))
+            {
+                print(ADMIN);
+                write(1, &character, 1);
+            }
+            else if (!strcmp(to_lowercase(word), "scarecrow"))
+            {
+                print(SCARECROWN);
+                write(1, &character, 1);
+            }
+            else if (!strcmp(to_lowercase(word), "tinman"))
+            {
+                print(TINMAN);
+                write(1, &character, 1);
+            }
+            else if (!strcmp(to_lowercase(word), "lion"))
+            {
+                print(LION);
+                write(1, &character, 1);
+            }
+            else if (!strcmp(to_lowercase(word), "dog") || !strcmp(to_lowercase(word), "toto"))
+            {
+                print(DOG);
+                write(1, &character, 1);
+            }
+            else if (!strcmp(to_lowercase(word), "trees"))
+            {
+                print(TREES);
+                write(1, &character, 1);
+            }
+            else if (!strcmp(to_lowercase(word), "guardian"))
+            {
+                print(GUARDIAN);
+                write(1, &character, 1);
+            }
+            else if (!strcmp(to_lowercase(word), "ghost") || !strcmp(to_lowercase(word), "casper"))
+            {
+                print(GHOST);
+                write(1, &character, 1);
+            }
+            else if (is_command(to_lowercase(word)))
+            {
+                if (!strcmp(to_lowercase(word), "cat"))
+                {
+                    print(concat(ANSI_COLOR_MAGENTA, "CAT"));
+                }
+                else if (!strcmp(to_lowercase(word), "cp"))
+                {
+                    print(concat(ANSI_COLOR_MAGENTA, "CP"));
+                }
+                else if (!strcmp(to_lowercase(word), "grep"))
+                {
+                    print(concat(ANSI_COLOR_MAGENTA, "GREP"));
+                }
+                else if (!strcmp(to_lowercase(word), "help"))
+                {
+                    print(concat(ANSI_COLOR_GREEN, "HELP"));
+                }
+                else if (!strcmp(to_lowercase(word), "ls"))
+                {
+                    print(concat(ANSI_COLOR_YELLOW, "LS"));
+                }
+                else if (!strcmp(to_lowercase(word), "man"))
+                {
+                    print(concat(ANSI_COLOR_GREEN, "MAN"));
+                }
+                else if (!strcmp(to_lowercase(word), "mv"))
+                {
+                    print(concat(ANSI_COLOR_MAGENTA, "MV"));
+                }
+                else if (!strcmp(to_lowercase(word), "pwd"))
+                {
+                    print(concat(ANSI_COLOR_YELLOW, "PWD"));
+                }
+                else if (!strcmp(to_lowercase(word), "touch"))
+                {
+                    print(concat(ANSI_COLOR_BLUE, "TOUCH"));
+                }
+                else if (!strcmp(to_lowercase(word), "stee"))
+                {
+                    print(concat(ANSI_COLOR_WHITE, "STEE"));
+                }
+                else if (!strcmp(to_lowercase(word), "exit"))
+                {
+                    print(concat(ANSI_COLOR_RED, "EXIT"));
+                }
+                else if (!strcmp(to_lowercase(word), "cd"))
+                {
+                    print(concat(ANSI_COLOR_CYAN, "CD"));
+                }
+
+                write(1, &character, 1);
+            }
+            else
+            {
+                print(split[j]);
+            }
+        }
+
+        if (character == '.' || character == '!' || character == '?')
+        {
+            println(bold(" << Press ENTER key to continue... >> "));
+            wait_until_enter();
+        }
+        else
+        {
+            print(" ");
         }
     }
 
     close(main_fd);
-    close(secd_fd);
+
+    println(" ");
 
     return 0;
 }
